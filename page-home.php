c30f9810820cd8382bc2e4b22ded6ae8757e9ea7
ernesdumani <ernesdumani@gmail.com>
2025-04-28 17:45:29 +0200

    ernes
    added stuff

--- page-home.php
+++ page-home.php
@@ -48,19 +48,7 @@
                                 if( $postlist->have_posts() ):
                                     while( $postlist->have_posts() ) : $postlist->the_post();
                                     ?>
-                                    <article class="latest-news">
-                                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'large' ); ?></a>
-                                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                                        <div class="meta-info">
-                                        <p>
-                                            by <span><?php the_author_posts_link(); ?></span> 
-                                            Categories: <span><?php the_category( ' ' ); ?></span>
-                                            Tags: <?php the_tags( '', ', ' ); ?>
-                                        </p>
-                                        <p><span><?php echo get_the_date(); ?></p>
-                                        </div>
-                                        <?php the_excerpt(); ?>
-                                    </article>
+                                
                                     <?php
                                     endwhile;
                                     wp_reset_postdata();
